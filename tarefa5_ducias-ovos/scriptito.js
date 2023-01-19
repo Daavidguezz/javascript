5c84f300ac37a4937a939cb63366e23942a548b2
Daavidguezz <92926097+Daavidguezz@users.noreply.github.com>
2023-01-19 22:59:28 +0100

    Update scriptito.js

--- tarefa5_ducias-ovos/scriptito.js
+++ tarefa5_ducias-ovos/scriptito.js
@@ -3,9 +3,9 @@ docenas= prompt("Buenas! Cuantas docensa lleva tu tortilla?");
 var numero= parseInt(docenas);
 alert("Has escrito "+numero);
 var huevos= numero*12
-alert("Tu tortilla lleva "+ huevos + " huevos")
+alert("Tu tortilla lleva "+ huevos + " huevos");
 
-document.write ("Tu tortilla lleva: "+docenas +" docenas" +"<br><br>")
+document.write ("Tu tortilla lleva: "+docenas +" docenas" +"<br><br>");
 
-document.write ("Tu tortilla lleva: "+huevos +" huevos")
+document.write ("Tu tortilla lleva: "+huevos +" huevos");
 
